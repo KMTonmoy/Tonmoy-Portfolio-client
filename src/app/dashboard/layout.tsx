b82394c0bcb43d5ab1e3ecf388553d1c1d95a4c3
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-02-11 08:18:11 +0600

    Added

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,15 @@
+import React, { ReactNode } from 'react';
+import Sidebar from '@/components/Sidebar/Sidebar';
+import '../../app/globals.css';
 
-import Sidebar from "@/components/Sidebar/Sidebar";
-import "../../app/globals.css";
+interface RootLayoutProps {
+    children: ReactNode;
+}
 
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <div>
-            <div className="flex ">
+            <div className="flex">
                 <Sidebar />
                 <main className="flex justify-center w-full">{children}</main>
             </div>
